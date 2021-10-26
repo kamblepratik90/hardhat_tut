f0d6e0838c0131f3bcad2553dd7e36c27ca350b8
kamblepratik90 <kamblepratik.90@gmail.com>
2021-10-26 14:06:46 +0530

    verify publish contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,6 @@
 require('@nomiclabs/hardhat-waffle');
+require("@nomiclabs/hardhat-solhint");
+require("@nomiclabs/hardhat-etherscan");
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
@@ -15,6 +17,13 @@ module.exports = {
     matic: {
       url: "https://rpc-mumbai.maticvigil.com",
       accounts: ["ce25ea05a902ba501d04da1594f587bf8ec7a7154a38ae5d04d0ff5806eefe3f"]
-    }
+    },
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/W7xdeF1I5yzryiFdqL8ziMqGCZrRQrnQ",
+      accounts: ["ce25ea05a902ba501d04da1594f587bf8ec7a7154a38ae5d04d0ff5806eefe3f"]
+    },
+  },
+  etherscan: {
+    apiKey: "W99AY3HJM13ATDE5IIJ8HFRCRBR56EU4S3",
   }
 };
